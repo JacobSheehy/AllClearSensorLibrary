dd56040dabe865411395d84d8b67254b4bb83c52
Jacob Sheehy <jacob.sheehy@gmail.com>
2018-06-23 14:22:03 -0600

    delete old file on overwrite

--- sensorlibrary/src/main/java/com/allclearweather/sensorlibrary/util/FileUtil.kt
+++ sensorlibrary/src/main/java/com/allclearweather/sensorlibrary/util/FileUtil.kt
@@ -14,12 +14,17 @@ class FileUtil {
 
         val measurementBufferMax = 1500
 
+        fun log(text: String) {
+            println(text)
+        }
+
         fun fileExists(context: Context, filename: String): Boolean {
             val file = context.getFileStreamPath(filename)
             return !(file == null || !file.exists())
         }
 
         fun cleanOldFile(context: Context, fileName: String) {
+            log("running cleanoldfile");
             if(!fileExists(context, fileName)) {
                 InternalConfig.log("$fileName does not exist for cleanOldFile")
                 return
@@ -28,25 +33,32 @@ class FileUtil {
             var newFile = ""
             val fileContents = readFile(context, fileName)
             var sensorData = fileContents.lines()
-
+            log("${sensorData.size} lines read from file contents")
             if(sensorData.size> measurementBufferMax) {
                 sensorData = sensorData.subList(Math.min(10, sensorData.size), sensorData.size)
+                log("${sensorData.size} new lines formed")
                 for (sensorD in sensorData) {
                     if(sensorD.length>2) {
                         newFile = "$newFile$sensorD\n"
                     }
                 }
                 saveFileOverwrite(context, fileName, newFile)
+            } else {
+                println("${sensorData.size} is not more than $measurementBufferMax so not overwriting file");
             }
         }
 
         fun saveFile(context: Context, fileName: String, fileContents: String) {
+            log("sensorlibrary save file")
             context.openFileOutput(fileName, Context.MODE_APPEND).use {
                 it.write(fileContents.toByteArray())
             }
         }
 
         fun saveFileOverwrite(context: Context, fileName: String, fileContents: String) {
+            log("sensorlibrary running savefileoverwrite");
+            val file = context.getFileStreamPath(fileName)
+            file.delete()
             context.openFileOutput(fileName, Context.MODE_PRIVATE).use {
                 it.write(fileContents.toByteArray())
             }
@@ -54,6 +66,7 @@ class FileUtil {
 
 
         fun readFile(context: Context, fileName: String) : String {
+            log("sensorlibrary running readfile")
             var content = ""
             context.openFileInput(fileName).use {
                 val input = ByteArray(it.available())
