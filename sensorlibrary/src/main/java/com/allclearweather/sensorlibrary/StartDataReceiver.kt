9af36a0c4f2d5884de80db7f080ce7ffe44ae6db
Jacob Sheehy <jacob.sheehy@gmail.com>
2018-08-19 08:34:36 -0600

    reverting back from androidx to support libs

--- sensorlibrary/src/main/java/com/allclearweather/sensorlibrary/StartDataReceiver.kt
+++ sensorlibrary/src/main/java/com/allclearweather/sensorlibrary/StartDataReceiver.kt
@@ -4,7 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import androidx.core.content.ContextCompat
+import android.support.v4.content.ContextCompat
 
 /**
  * Receive intents from other apps / host apps and send them to the service to start.
